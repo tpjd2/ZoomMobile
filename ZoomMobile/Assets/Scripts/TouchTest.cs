784202f488ca426a3a132ed13b03f800ea379a03
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-08-02 17:16:24 -0300

    implementação do rastreamento de toques na tela

---
+++ ZoomMobile/Assets/Scripts/TouchTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour
+{
+    [SerializeField] int frequencia;
+
+    Touch[] toques;
+    Touch toque;
+    float tempo;
+    int delta, aux;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempo = 0;
+        delta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        aux = Mathf.FloorToInt(tempo);
+        if (aux > delta)
+        {
+            delta = aux + frequencia;
+            toques = Input.touches;
+
+            foreach(Touch t in toques)
+            {
+                Debug.Log("ID: " + t.fingerId + " (" + t.position.x + ", " + t.position.y + ")");
+            }
+            /*
+            if (Input.touchCount > 0)
+            {
+                toque = Input.GetTouch(0);
+                Debug.Log("(" + toque.position.x + ", " + toque.position.y + ")");
+            }
+            */
+        }        
+    }
+}
