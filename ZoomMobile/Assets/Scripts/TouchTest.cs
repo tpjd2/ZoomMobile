2ae53e3d3a1fbc707ee0e23ce6a05b2dc9e1b791
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-08-14 14:46:37 -0300

    Script para realizar zoom com o movimento de pinça

--- ZoomMobile/Assets/Scripts/TouchTest.cs
+++ ZoomMobile/Assets/Scripts/TouchTest.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class TouchTest : MonoBehaviour
 {
+    [SerializeField] float velZoomOrthographic;
+    [SerializeField] float velZoomPerspective;
     [SerializeField] int frequencia;
 
+    Camera camera;
     Touch[] toques;
     Touch toque;
     float tempo;
@@ -16,6 +19,7 @@ public class TouchTest : MonoBehaviour
     {
         tempo = 0;
         delta = 0;
+        camera = Camera.main;
     }
 
     // Update is called once per frame
@@ -32,13 +36,38 @@ public class TouchTest : MonoBehaviour
             {
                 Debug.Log("ID: " + t.fingerId + " (" + t.position.x + ", " + t.position.y + ")");
             }
-            /*
-            if (Input.touchCount > 0)
-            {
-                toque = Input.GetTouch(0);
-                Debug.Log("(" + toque.position.x + ", " + toque.position.y + ")");
-            }
-            */
-        }        
+        }
+        
+        // DETECCAO DE MOVIMENTO DE PINCA
+        if (Input.touchCount == 2)
+        {
+            Zoom();
+        }
+    }
+
+    void Zoom()
+    {
+        Touch toqueZero = Input.GetTouch(0);
+        Touch toqueUm = Input.GetTouch(1);
+
+        Vector2 PosAntToqueZero = toqueZero.position - toqueZero.deltaPosition;
+        Vector2 PosAntToqueUm = toqueUm.position - toqueUm.deltaPosition;
+
+        float DistAnterior = (PosAntToqueZero - PosAntToqueUm).magnitude;
+        float DistAgora = (toqueZero.position - toqueUm.position).magnitude;
+
+        float DeltaDistancia = DistAnterior - DistAgora;
+
+        if (camera.orthographic)
+        {
+            camera.orthographicSize += DeltaDistancia * velZoomOrthographic;
+            camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.1f);
+            camera.orthographicSize = Mathf.Min(camera.orthographicSize, 10f);
+        }
+        else
+        {
+            camera.fieldOfView += DeltaDistancia * velZoomPerspective;
+            camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, 0.1f, 179.9f);
+        }
     }
 }
